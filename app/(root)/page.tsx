d3854847e2543569bbf3f9c9fb764393b708707d
Vitalik <burbanvitalik2002@gmail.com>
2025-04-02 14:37:45 +0300

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,62 @@
+import {
+  Container,
+  Filters,
+  Title,
+  TopBar,
+  ProductsGroupList,
+  Stories,
+} from "@/shared/components/shared";
+import { Suspense } from "react";
+import { GetSearchParams, findPizzas } from "@/shared/lib/find-pizzas";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: GetSearchParams;
+}) {
+  const categories = await findPizzas(searchParams);
+
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
+
+      <TopBar
+        categories={categories.filter(
+          (category) => category.products.length > 0
+        )}
+      />
+
+      {/* <div className="max-w-[90%] overflow-x-hidden mx-auto">
+        <Stories />
+      </div> */}
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[80px]">
+          <div className="w-[250px]">
+            <Suspense>
+              <Filters />
+            </Suspense>
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  )
+              )}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
